60cff941f112ea6a86d3eb3ad82962a63b8a0e53
Quentin Peter <quentin.peter@epfl.ch>
2015-03-12 10:20:33 +0100

    Added file

---
+++ nanHighStd.m
@@ -0,0 +1,11 @@
+function data = nanHighStd(data)
+
+%Keep lines with STD < 3* median to scale
+stdData= nanstd(data,0,2);
+stdCut=3*nanmedian(stdData);
+
+%Cut to keep "good lines"
+linesSTDev= stdData > stdCut;
+linesSTDev = logical(linesSTDev*ones([1 size(data,2)]));%matrix size
+data(linesSTDev)=0;
+end
