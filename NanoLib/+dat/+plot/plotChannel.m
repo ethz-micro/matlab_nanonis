ae9f502a0dbf8c61abf586da5683ed87aeaa2d80
Danilo A. Zanin <dzanin@phys.ethz.ch>
2016-09-05 13:21:15 +0200

    switched x_channel, y_channel!!!! -> check examples. Help added

--- NanoLib/+dat/+plot/plotChannel.m
+++ NanoLib/+dat/+plot/plotChannel.m
@@ -1,19 +1,54 @@
-function hObject = plotChannel(y_channel,x_channel,varargin)
-% hObject = plotChannel(channel,x_channel,varargin)
+function hObject = plotChannel(x_channel,y_channel,varargin)
+%PLOTCHANNEL - plots ONE y_channel vs the x_channel, both are channels
+%    of the file loaded with the function dat.load.loadProcessedDat
 %
-% varargin are the same as for normal matlab plot function
-% additional options are:
-%      varargin = {'xOffset',NUMBER} : shift of the x axis 
-%      varargin = {'hideLabels'}     : leave labels out
+% Syntax: 
+%   hObject = PLOTCHANNEL(x_channel)
+%   hObject = PLOTCHANNEL(x_channel,y_channel)
+%   hObject = PLOTCHANNEL(x_channel,y_channel,varargin)
+%
+% Inputs:
+%    x_channel - channel relative to the x-axis. If only the x_channel is 
+%                given, it is plotted vs the number of points.
+%    y_channel - channel relative to the y-axis
+%    varargin - are the same as for starndard matlab plot function
+%       additional options are the same as dat.plot.plotData.m
+%
+% Outputs:
+%    hObject - figure handle
+%
+% Example: 
+%   file = dat.load.loadProcessedDat(fn);
+%   hObj = dat.plot.PLOTCHANNEL(file.channels(1),file.channels(2));
+%
+% See also dat.plot.plotData.m
 
-if numel(y_channel) > 1
-    error('plotchannel only plots 1 channel at the time. %d provided',numel(y_channel));
+% September 2016
+
+%------------- BEGIN CODE --------------
+
+% check number of argument in
+narginchk(1,inf);
+
+% check if if x_channel is given as argument
+xGiven = false;
+if nargin == 1
+    xGiven = true;
+elseif ~isstruct(y_channel)
+    xGiven = true;
+    varargin = {y_channel,varargin{:}};
+end
+if xGiven
+    y_channel = x_channel;
+    x_channel.Name = 'pti';
+    x_channel.Unit = ' ';
+    x_channel.Direction = y_channel.Direction;
+    x_channel.data = repmat(1:size(y_channel.data,1),size(y_channel.data,2),1)';
 end
 
-% one may provide the file insted of the x_channel
-% in this case the first channel is the x_channel
-if isfield(x_channel,'header')
-    x_channel = x_channel.channels(1);
+% check if more than 1 channel given
+if numel(y_channel) > 1
+    error('plotchannel only plots 1 channel at the time. %d provided',numel(y_channel));
 end
 
 name = sprintf('%s - %s',y_channel.Name,y_channel.Direction);
