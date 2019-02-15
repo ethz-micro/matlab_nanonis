1eff23858279a446ce6d57ab23eb12b2f295992e
garghi2000 <garghi2000@hotmail.it>
2019-02-15 15:49:02 +0100

    Fixed bug when a bias spectroscopy with more sweeps is done,now it shows the average for every channel named with an extra [AVG]

--- NanoLib/+dat/+load/experiment_biasSpectroscopy.m
+++ NanoLib/+dat/+load/experiment_biasSpectroscopy.m
@@ -37,10 +37,11 @@ header.sweep_signal = 'Bias (V)';
 
 end
 
-% process Data
+% process Data(in case of more loops it shows only the average)
 function [header,channels] = processData(header,data)
 channels = struct;
 i = 1; % first is time
+j = 1; % j counts over the averaged channels when the file contains more than 1 loop
 chnName = strsplit(header.channels{i},{'(',')'});
 channels(i).Name = strtrim(chnName{1});
 channels(i).Unit = chnName{2};
@@ -48,21 +49,24 @@ channels(i).Direction = 'both';
 channels(i).data = data(:,i);
 
 for i = 2:size(data,2);
-    ib = strfind(header.channels{i},'[bwd]');
-    if isempty(ib)
-        chnName = strsplit(header.channels{i},{'(',')'});
-        channels(i).Name = strcat(chnName{1},chnName{3});
-        channels(i).Unit = chnName{2};
-        channels(i).Direction = 'forward';
-    else
-        chnName = header.channels{i};
-        chnName(ib:ib+5) = [];
-        chnName = strsplit(chnName,{'(',')'});
-        channels(i).Name = strcat(chnName{1},chnName{3});
-        channels(i).Unit = chnName{2};
-        channels(i).Direction = 'backward';
-        channels(i).data = data(:,i);
+    iloops = strfind(header.channels{i},'[0');%it checks if there are less than 9999 loops
+    if isempty(iloops) %when the channel is not one of the loops it save it otherwise no
+        j = j + 1 ;
+        ib = strfind(header.channels{i},'[bwd]');
+        if isempty(ib)
+            chnName = strsplit(header.channels{i},{'(',')'});
+            channels(j).Name = strcat(chnName{1},chnName{3});
+            channels(j).Unit = chnName{2};
+            channels(j).Direction = 'forward';
+        else
+            chnName = header.channels{i};
+            chnName(ib:ib+5) = [];
+            chnName = strsplit(chnName,{'(',')'});
+            channels(j).Name = strcat(chnName{1},chnName{3});
+            channels(j).Unit = chnName{2};
+            channels(j).Direction = 'backward';
+        end
+        channels(j).data = data(:,i);
     end
-    channels(i).data = data(:,i); 
 end
 end
